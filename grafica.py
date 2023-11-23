28c903120a29185751498f5c922a72fc2b740df1
AquiahuatlAo <jaquiahuatlo1500@alumno.ipn.mx>
2023-11-23 17:09:30 -0600

    comentarios
    Se agregan comentarios en el documento

--- grafica.py
+++ grafica.py
@@ -6,6 +6,9 @@ x=[2.8, 6.5, 2.5, 5.6, 5.5, 4.6, 3.6, 2.0]
 y=[4.8, 5.5, 3.5, 4.6, 6.5, 6.6, 2.6, 3.0]
 
 #plot 
+#Se agrega comentario para realise
+print("Impresion en branch realise)
+#Se agregan nuevos comentarios en la rama de comments
 
 plt.title('Grafica en png')
 plt.savefig("graficaJPG.png")
